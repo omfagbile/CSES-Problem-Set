bd1b84cd4936ab48c2455bc0a204023ab30a1736
omfagbile <olu.martin7@gmail.com>
2025-11-21 19:39:36 +0000

    Update repetitions.py

--- 0. Introductory Problems/repetitions.py
+++ 0. Introductory Problems/repetitions.py
@@ -4,8 +4,9 @@ n = len(dna)
 ans = 0 
 
 for i in range(n):
-  j = 0 #end pointer
-  while dna[i] == dna[j]:
+  j = i #end pointer
+  while j+1<=n-1 and dna[i] == dna[j+1]:
     j += 1
-  ans = max(ans, j-i)
+  ans = max(ans, j-i+1)
 
+print(ans)
